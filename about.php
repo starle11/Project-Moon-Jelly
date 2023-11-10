1aa1b289d1ba5df45c8e7559a41fd1ac54b6866e
Raymondmateo <150483242+Raymondmateo@users.noreply.github.com>
2023-11-10 15:50:23 -0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Us</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>About Us</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="fc.php">Create Flashcards</a></li>
+        </ul>
+    </nav>
+    <main>
+        <p>This is the About page.</p>
+    </main>
+</body>
+</html>
